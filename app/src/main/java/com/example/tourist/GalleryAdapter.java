b294085bb6491582547c1b8571f351d39c1cbca6
Hadi Farhat <hfarhat1@sheffield.ac.uk>
2020-12-22 16:16:20 +0000

    Load a thumbnail instead of loading an image

--- app/src/main/java/com/example/tourist/GalleryAdapter.java
+++ app/src/main/java/com/example/tourist/GalleryAdapter.java
@@ -60,7 +60,8 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.View_Hol
             if (items.get(position).image != -1) {
                 holder.imageView.setImageResource(items.get(position).image);
             } else if (items.get(position).file != null) {
-                Bitmap bitmap = BitmapFactory.decodeFile(items.get(position).file.getAbsolutePath());
+                Bitmap bitmap = decodeSampledBitmapFromResource(
+                        items.get(position).file.getAbsolutePath(), 150, 150);
                 holder.imageView.setImageBitmap(bitmap);
             }
 
@@ -78,5 +79,37 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.View_Hol
     @Override
     public int getItemCount() {
         return items.size();
+    }
+
+
+    public static Bitmap decodeSampledBitmapFromResource(String filePath, int reqWidth, int reqHeight) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 }
