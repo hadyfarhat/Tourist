bdbca5fe793612141cde68d0560ebaf9ea7ef38e
Hadi Farhat <hfarhat1@sheffield.ac.uk>
2021-01-03 20:38:05 +0000

    create a Adapter helper class to help with decoding bitmap files accross adapters

--- app/src/main/java/com/example/tourist/view/adapter/MomentAdapter.java
+++ app/src/main/java/com/example/tourist/view/adapter/MomentAdapter.java
@@ -123,39 +123,6 @@ public class MomentAdapter extends RecyclerView.Adapter<MomentAdapter.View_Holde
     }
 
 
-    public static Bitmap decodeSampledBitmapFromResource(String filePath, int reqWidth, int reqHeight) {
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(filePath, options);
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(filePath, options);
-    }
-
-
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-        if (height > reqHeight || width > reqWidth) {
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-        return inSampleSize;
-    }
-
-
     /**
      * Creates a bitmap based on the file provided.
      * It then uses this bitmap to set the image of the view holder's image item
@@ -168,8 +135,10 @@ public class MomentAdapter extends RecyclerView.Adapter<MomentAdapter.View_Holde
         protected Bitmap doInBackground(HolderAndPosition... holderAndPosition) {
             holderAndPositionReference = new WeakReference<>(holderAndPosition[0]);
             HolderAndPosition holdAndPos = holderAndPositionReference.get();
-            Bitmap bitmap = decodeSampledBitmapFromResource(
+            Bitmap bitmap = AdapterHelper.decodeSampledBitmapFromResource(
                     mMoments.get(holdAndPos.position).getImageFilePath(), 230, 270);
+            //Bitmap bitmap = decodeSampledBitmapFromResource(
+            //        mMoments.get(holdAndPos.position).getImageFilePath(), 230, 270);
             return bitmap;
         }
 
